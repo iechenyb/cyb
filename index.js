49d4f697a1e0b14cd8b4fb828cff2c73fc2120e4
iechenyb <zzuchenyb@sina.com>
2017-02-16 17:06:42 +0800

    add

--- index.js
+++ index.js
@@ -16,16 +16,50 @@ myApp.config(['$stateProvider', '$urlRouterProvider','$httpProvider',
             url:"/inner",
             templateUrl:"p1/infor/index.html",
             controller:function($scope,$http,$rootScope,$filter){
-                $scope.start=1;
-                $scope.end=pageSize*1;
-            $scope.refresh=function(){
-                $scope.result = $filter("filter")($scope.list,{name:$scope.q1,work:$scope.q2});
-                $scope.total = $scope.result.length;
-                $scope.pages = Math.ceil($scope.result.length / pageSize); //得到总页数
-                $scope.cur=1;
-                $scope.start=1;
-                $scope.end=pageSize*1;
-                $("#page").page({
+                var $page;
+                $scope.$watch("res",function () {
+                    $scope.cur=1;
+                    $scope.start=1;
+                    $scope.end=pageSize*1;
+                    renderSpilter($scope.result,$scope);
+                });
+                $scope.refresh=function(){
+                    $scope.result = $filter("filter")($scope.list,{name:$scope.q1,work:$scope.q2});
+                    /*$scope.total = $scope.result.length;
+                    $scope.pages = Math.ceil($scope.result.length / pageSize); //得到总页数
+                    $scope.cur=1;
+                    $scope.start=1;
+                    $scope.end=pageSize*1;*/
+               /* $page=$("#page").page({
+                    pages:$scope.pages, //页数
+                    curr: $scope.cur //当前页
+                    /!*type: 'default', //主题
+                    groups: 5, //连续显示分页数
+                    prev: '<', //若不显示，设置false即可
+                    next: '>', //若不显示，设置false即可
+                    //first: "首页",
+                    //last: "尾页", //false则不显示
+                    jump: function(context, first) {
+                        if(first) return;
+                        console.log('当前第#：' + context.option.curr + "页,pages="+$scope.pages);
+                        $scope.$apply(function () {
+                            $scope.cur = context.option.curr;
+                            $scope.start = ($scope.cur-1)*pageSize+1;
+                            $scope.end = $scope.cur*pageSize;
+                        });
+                    }*!/
+                });*/
+               /* $page.remove(function() {
+                    console.log('移除分页组件成功');
+                });*/
+                //$scope.$apply();
+            };
+            $http.get("data/infor/list.json").success(function(data){
+                $scope.result = $scope.list = data;
+               /* $scope.total =  $scope.result.length;
+                $scope.pages = Math.ceil($scope.result.length / pageSize); //得到总页数*/
+                renderSpilter(data,$scope);
+                /*$page=$("#page").page({
                     pages:$scope.pages, //页数
                     curr: $scope.cur, //当前页
                     type: 'default', //主题
@@ -35,37 +69,11 @@ myApp.config(['$stateProvider', '$urlRouterProvider','$httpProvider',
                     first: "首页",
                     last: "尾页", //false则不显示
                     jump: function(context, first) {
-                        if(first) return;
-                        console.log('当前第：' + context.option.curr + "页");
-                        $scope.$apply(function () {
-                            $scope.cur = context.option.curr;
-                            $scope.start = ($scope.cur-1)*pageSize+1;
-                            $scope.end = $scope.cur*pageSize;
-                        });
-                    }
-                });
-                //$scope.$apply();
-            };
-            $http.get("data/infor/list.json").success(function(data){
-                $scope.result = $scope.list = data;
-                $scope.total =  $scope.result.length;
-                $scope.pages = Math.ceil($scope.list.length / pageSize); //得到总页数
-                page.cur=1;
-                $("#page").page({
-                    pages:$scope.pages, //页数
-                    curr: page.cur, //当前页
-                    type: 'default', //主题
-                    groups: 5, //连续显示分页数
-                    prev: '<', //若不显示，设置false即可
-                    next: '>', //若不显示，设置false即可
-                    first: "首页",
-                    last: "尾页", //false则不显示
-                    jump: function(context, first) {
                         if (first)return;
                         console.log('当前第：' + context.option.curr + "页");
-                       /* $scope.cur = context.option.curr;
+                       /!* $scope.cur = context.option.curr;
                         $scope.start = ($scope.cur-1)*pageSize+1;
-                        $scope.end = $scope.cur*pageSize;*/
+                        $scope.end = $scope.cur*pageSize;*!/
                         $scope.$apply(function () {
                             $scope.cur = context.option.curr;
                             $scope.start = ($scope.cur-1)*pageSize+1;
@@ -73,10 +81,11 @@ myApp.config(['$stateProvider', '$urlRouterProvider','$httpProvider',
                         });
                         //$scope.$apply();
                     }
-                });
+                });*/
             });
             }
         });
+        //$scope.$apply();
         $urlRouterProvider.otherwise("/exception/404");
     }
 ]).controller("myCtrl",function($scope,$http){
