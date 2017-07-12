c7959f63352d53bc6cd7965cc4aa816f6109a20b
iechenyb <zzuchenyb@sina.com>
2017-07-12 22:01:14 +0800

    更新智能化

--- game/wzq1/pub.js
+++ game/wzq1/pub.js
@@ -17,5 +17,25 @@ win4Str[1]="****";
 var win3Str = new Array(2);
 win3Str[0]="###";
 win3Str[1]="***";
+var win2Str = new Array(2);
+win2Str[0]="##";
+win2Str[1]="**";
+var win1Str = new Array(2);
+win1Str[0]="#";
+win1Str[1]="*";
 var over = false;
 var winPeron = -1 ;
+var map0 = new Map();
+var map1 = new Map();
+var nullArr=new Array();
+//只需要存机器下一步 乙方
+//下步可以走  乙方
+map0.put("one",nullArr);
+map0.put("two",nullArr);
+map0.put("three",nullArr);
+map0.put("four",nullArr);
+//下步可以走 甲方
+map1.put("one",nullArr);
+map1.put("two",nullArr);
+map1.put("three",nullArr);
+map1.put("four",nullArr);
