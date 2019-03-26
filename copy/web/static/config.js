7bdb51f01f7fb9a6584413a28e278dc36fdedd4a
iechenyb <zzuchenyb@sina.com>
2019-03-26 08:27:31 +0800

    文本复制与cvs-chart

---
+++ copy/web/static/config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+  URL: 'http://192.168.108.224:8085/vasweb', // 配置AJAX_URL
+  // mode: 'history', // 路由方式
+  // V: 120, // 配置默认请求数
+  // CACHETIME: 60, // 配置数据缓存时长（s）
+  // TIMEOUT: 1000, // 配置请求超时时间
+};
